723b006acf70a13899dc63f8aca0734e5d7029f5
Ewa Oszajec <oszaj003@umn.edu>
2019-10-22 23:08:29 -0500

    proposed  solution to display the response. The proposed solution: 1. get response from userInput compnent 2. store response in app.js state 3. send response to DisplayResponse component
    Solves #9

---
+++ src/components/DisplayResponse.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class DisplayResponse extends Component {
+
+  render(){
+    return (
+      <div className="displayResponse">
+        <h3> The current response is ... </h3>
+        <p> {this.props.response} </p>
+      </div>
+    )
+  }
+}
+
+const displayResponse = {
+  display: 'inline-block'
+}
+
+export default DisplayResponse;
