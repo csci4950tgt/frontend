feb292c8e7da05a6ac9222ef22e01a64a7dd948b
ZeyuGao1 <43509561+ZeyuGao1@users.noreply.github.com>
2019-10-28 12:47:33 -0500

    Build a search component
    Build a search component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,26 @@
+import React from 'react';
 
+class SearchBar extends React.Component {
+	state = { term: '' };
+
+	onFormSubmit = event => {
+		event.preventDefault();
+
+		this.props.onSubmit(this.state.term);
+	};
+
+	render() {
+		return (
+				<form onSubmit={this.onFormSubmit}>
+						<label>Search Bar</label>
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={e => this.setState({ term: e.target.value })}
+						/>
+				</form>
+		);
+	}
+}
+
+export default SearchBar;
