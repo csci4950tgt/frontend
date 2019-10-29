3190f0bfe630bdd1633a61b8313bbb71c5237811
Yuze Jiang <fortissimoX@gmail.com>
2019-10-29 15:29:28 -0500

    Handle communication to backend correctly

--- src/apis/rest.js
+++ src/apis/rest.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export default axios.create({
-	baseURL: 'http://localhost:8080'
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
 });
+
+export default axiosInstance;
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import axiosInstance from '../apis/rest.js';
 
 class UserInput extends Component {
   state = {
@@ -8,79 +8,74 @@ class UserInput extends Component {
     screenshot: {
       height: '',
       width: '',
-      filename: 'screenshot.png'
-    }
-
-
-  }
+      filename: 'screenshot.png',
+    },
+  };
   //for multiple on forms on a field
-  onChange = (e) => {
-    if(e.target.name == "width" || e.target.name == "height"){
+  onChange = e => {
+    if (e.target.name == 'width' || e.target.name == 'height') {
       var screenshotProp = this.state.screenshot;
-      if(e.target.name == "width"){
-        screenshotProp.width = e.target.value;
+      if (e.target.name == 'width') {
+        screenshotProp.width = parseInt(e.target.value);
+      } else {
+        screenshotProp.height = parseInt(e.target.value);
       }
-      else{
-        screenshotProp.height = e.target.value;
-      }
-      this.setState({screenshotProp});
+      this.setState({ screenshotProp });
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
     }
-    else{
-      this.setState({[e.target.name]:e.target.value});
-    }
-  }
+  };
 
-  onFormSubmit = async (e) => {
+  onFormSubmit = async e => {
     e.preventDefault();
-    try{
-      const res = await fetch("/api/honeyclient/create", {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        body: JSON.stringify(this.state) // body data type must match "Content-Type" header'
-    })
-    const response = await res.json();
-    console.log(res);
-    console.log(JSON.stringify(response));
-    this.props.response(JSON.stringify(response));
-    }
-    catch(error){
-      console.log(error);
-    }
 
-  }
-  render(){
+    axiosInstance
+      .post('/api/honeyclient/create', this.state)
+      .then(function(response) {
+        console.log(JSON.stringify(response));
+        this.props.response(JSON.stringify(response));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  render() {
     return (
       <div>
-        <h3> Enter Parameters:</h3>
-        <form
-        onSubmit={this.onFormSubmit}
-        style={{display: 'inline-block'}}>
-        <input
-          style={inputStyle}
-          type="text"
-          name="url"
-          placeholder="Enter an url"
-          value={this.state.url}
-          onChange={this.onChange} />
-        <input
-          style={inputStyle}
-          type="text"
-          name="width"
-          placeholder="Enter an image width"
-          value={this.state.screenshot.width}
-          onChange={this.onChange} />
-        <input
-          style={inputStyle}
-          type="text"
-          name="height"
-          placeholder=" Enter an image height"
-          value={this.state.screenshot.height}
-          onChange={this.onChange} />
-        <input
-          type="submit"
-          value="Submit"
-          className="btn"
-          style={{flex: '1'}}/>
-      </form>
+        <h3>Enter Parameters:</h3>
+        <form onSubmit={this.onFormSubmit} style={{ display: 'inline-block' }}>
+          <input
+            style={inputStyle}
+            type="text"
+            name="url"
+            placeholder="Enter an url"
+            value={this.state.url}
+            onChange={this.onChange}
+          />
+          <input
+            style={inputStyle}
+            type="text"
+            name="width"
+            placeholder="Enter an image width"
+            value={this.state.screenshot.width}
+            onChange={this.onChange}
+          />
+          <input
+            style={inputStyle}
+            type="text"
+            name="height"
+            placeholder="Enter an image height"
+            value={this.state.screenshot.height}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="btn"
+            style={{ flex: '1' }}
+          />
+        </form>
       </div>
     );
   }
@@ -90,7 +85,7 @@ const inputStyle = {
   display: 'block',
   flex: '10',
   padding: '5px',
-  margin: '10px'
-}
+  margin: '10px',
+};
 
 export default UserInput;
