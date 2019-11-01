4c97a878f375850a1975e246e99a539213ad53d8
Rafael Barash <rafabarash@gmail.com>
2019-11-01 16:41:11 -0500

    added prettier script to package.json, can run with "npm run prettier"

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import './App.css';
 import UserInput from './components/UserInput';
 import DisplayResponse from './components/DisplayResponse';
 
-
 class App extends Component {
-
   state = {
     responseData: null,
-    ticket: {}
-  }
+    ticket: {},
+  };
 
-  myCallback = (dataFromResponse) =>{
-    this.setState({responseData : dataFromResponse})
+  myCallback = dataFromResponse => {
+    this.setState({ responseData: dataFromResponse });
     //a test
-  }
+  };
 
-
-
-
-  render(){
+  render() {
     return (
-      <div className="App" >
+      <div className="App">
         <h1> Vigilante Web Heist</h1>
 
-        <UserInput response={this.myCallback}/>
+        <UserInput response={this.myCallback} />
 
-        {this.state && this.state.responseData &&
-          <DisplayResponse response={this.state.responseData}/>
-        }
-
+        {this.state && this.state.responseData && (
+          <DisplayResponse response={this.state.responseData} />
+        )}
       </div>
     );
   }
