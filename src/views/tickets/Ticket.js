b47df2860bfa9feed967cbc421bf7645c2b6fcf9
Ewa Oszajec <oszaj003@umn.edu>
2019-11-17 00:18:49 -0600

    WIP screenshot_soln

--- src/views/tickets/Ticket.js
+++ src/views/tickets/Ticket.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Header } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 
+import Screenshot from './Screenshot';
+
 const Ticket = () => {
   const { ticketID } = useParams();
   // TODO: add screenshot component in return function, pass down ticketID
@@ -9,6 +11,7 @@ const Ticket = () => {
   return (
     <>
       <Header as="h3">Ticket: {ticketID}</Header>
+      <Screenshot ticketID={ticketID} />
     </>
   );
 };
