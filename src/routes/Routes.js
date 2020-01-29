16d2b2e9565e45ebad5557cc756a47169929e54e
ZeyuGao1 <43509561+ZeyuGao1@users.noreply.github.com>
2020-01-29 11:36:55 -0600

    Update Routes.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 // Views
 import Home from '../views/home/Home';
 import Tickets from '../views/tickets/Tickets';
 
+const NoMatch = ({ location }) => (
+  <div>
+    <h1> We're sorry! this page is currently unavailable. please try again later. </h1>
+    <h3>(No matching page for <code>{location.pathname})</code> </h3>
+  </div>
+)
+
 const Routes = () => (
   <Switch>
     <Route exact path="/" render={() => <Home />} />
     <Route path="/tickets" render={() => <Tickets />} />
+    <Route component={NoMatch} />
   </Switch>
 );
 
