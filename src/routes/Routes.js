d38256183c97617d20cebc98e3836033e8f9f664
William Teder <wteder@hydreon.com>
2020-02-03 17:46:41 -0600

    feat(404): add link to return home

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 // Views
 import Home from '../views/home/Home';
@@ -7,10 +7,16 @@ import Tickets from '../views/tickets/Tickets';
 
 const NoMatch = ({ location }) => (
   <div>
-    <h1> We're sorry! this page is currently unavailable. please try again later. </h1>
-    <h3>(No matching page for <code>{location.pathname})</code> </h3>
+    <h1>
+      {' '}
+      We're sorry! This page is currently unavailable. Please try again later.{' '}
+    </h1>
+    <h3>
+      (No matching page for <code>{location.pathname})</code>{' '}
+    </h3>
+    <Link to="/">Return to Homepage</Link>
   </div>
-)
+);
 
 const Routes = () => (
   <Switch>
