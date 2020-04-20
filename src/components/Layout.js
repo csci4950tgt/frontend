2b12ff46b9a363f29fa8f79d98770f5636425bc0
Ewa Oszajec <oszaj003@umn.edu>
2020-04-20 12:50:58 -0500

    first test  script

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import Header from './Header';
 import Footer from './Footer';
+import Add from './Add';
 
 const Layout = ({ children }) => (
   <Container textAlign="center" style={{ marginTop: '3rem' }}>
     <Header />
     {children}
     <Footer />
+    <div className="App">
+      <header className="App-header">
+        <Add />
+      </header>
+    </div>
   </Container>
 );
 
